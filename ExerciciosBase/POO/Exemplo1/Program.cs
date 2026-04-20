6f82ab000742ad2570fb2de0736da799dc2b3b54
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-04-20 18:06:47 -0300

    .

---
+++ ExerciciosBase/POO/Exemplo1/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Aluno
+{
+    public string Nome {get;set;}
+    public float Nota1 {get;set;}
+    public float Nota2 {get;set;}
+    public float Nota3 {get;set;}
+    public float Nota4 {get;set;}
+
+    public void MostrarDados()
+    {
+        Console.WriteLine("Nome: " + Nome);
+        Console.WriteLine("nota1: " + Nota1);
+        Console.WriteLine("nota2: " + Nota2);
+        Console.WriteLine("nota3: " + Nota3);
+        Console.WriteLine("nota4: " + Nota4);
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Aluno n = new Aluno();
+
+        Console.Write("Informe o nome do aluno: ");
+        n.Nome = Console.ReadLine() ?? string.Empty;
+        
+        Console.Write("Nota 1: ");
+        n.Nota1 = float.Parse(Console.ReadLine() ?? string.Empty);
+
+        Console.Write("Nota 2: ");
+        n.Nota2 = float.Parse(Console.ReadLine() ?? string.Empty);
+        
+        Console.Write("Nota 3: ");
+        n.Nota3 = float.Parse(Console.ReadLine() ?? string.Empty);
+
+        Console.Write("Nota 4: ");
+        n.Nota4 = float.Parse(Console.ReadLine() ?? string.Empty);
+        
+        n.MostrarDados();
+    
+    }
+}
